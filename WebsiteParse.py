f0f53a93a3c3993ea25c649a1fd1e256b0fbd8f9
gitson <hoangify@outlook.com>
2021-09-11 21:37:01 +1000

    Remove duplicates.

--- WebsiteParse.py
+++ WebsiteParse.py
@@ -2,7 +2,15 @@ from os import get_terminal_size, linesep
 from typing import Deque
 from bs4 import BeautifulSoup
 import requests
-import csv
+
+"""
+TODO Features:
+- Parse URL
+- Notify system (email)
+- Parse images
+- Fix signature of price_range function
+- Improve efficiency of check loop
+"""
 
 class WebsiteParse:
     def __init__(self,
@@ -12,15 +20,13 @@ class WebsiteParse:
         self.name = {}
 
         page = requests.get(self.url)
+        self.sheet['Name'] = []
         self.sheet['Price'] = []
         self.soup = BeautifulSoup(page.content, 'html.parser')
 
     def gumtree_parse(self, wordlist: str):
         wordlist = wordlist.split()
-        # for line in soup.find_all('span', class_='user-ad-price-new-design__price'):
-        #     self.sheet['Price'].append(line.get_text())
         
-        self.sheet['Name'] = []
         # Parse: Listing/title
         count = 0 
         for line in self.soup.find_all('span', class_='user-ad-row-new-design__title-span'):
@@ -31,7 +37,7 @@ class WebsiteParse:
             count += 1
                         
             # self.sheet['Name'].append(line.get_text())
-        
+    
         """
         TODO: 
         - Input low/high parameters into the signature of the function.
@@ -52,6 +58,10 @@ class WebsiteParse:
         return self.sheet
 
     def parse_price(self, count: int):
+        """
+        Parse price values from GumTree website.
+        TODO: Implement into GumTree parse function.
+        """
         self.sheet['Price'].append(self.soup.find_all('span', class_='user-ad-price-new-design__price')[count].get_text())
         return self.sheet
     
@@ -62,12 +72,10 @@ class WebsiteParse:
         Output: Update sheet with items within the price range.
 
         Filter out any items listed outside the price range specified.
-
-        TODO:
-        - Remove duplicates.
-        - Sort by price.
         """ 
+        
         while True:
+            # print('testing...')
             for i in range(len(self.sheet['Price'])):
                 try:
                     # If price value is outside of range then remove from dictionary.
@@ -90,21 +98,20 @@ class WebsiteParse:
     def remove_duplicates(self):
         """
         Remove duplicate entries from dictionary.
-        Using a counting sort algorithm. Complexity is 0(n+k).
-            - n is the size of an array
-            - k is the amount of different possible elements
-        """
-        # Intialise array
+        """    
         
-
-        for i, j in enumerate(self.sheet['Name']):
-            print(i, j)
-                    
-            
-        
-
-
-
+        while True:
+            for i in range(len(self.sheet['Name'])):
+                if i == 0:
+                    continue
+                else:
+                    if self.sheet['Name'][i] == self.sheet['Name'][i-1]:
+                        self.sheet['Name'].pop(i)
+                        self.sheet['Price'].pop(i)
+                        break
+            if i == len(self.sheet['Name']):
+                break
+        return self.sheet
 
 
 
